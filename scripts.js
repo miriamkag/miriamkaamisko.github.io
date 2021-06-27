ead4787ffdc7db26d8d8511ebf77528e60e03dc6
miriamkag <64091804+miriamkag@users.noreply.github.com>
2021-06-27 11:03:34 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 
 // Set the date we're counting down to
-var countDownDate = new Date("Jul 10, 2021 16:00:00").getTime();
+var countDownDate = new Date("Aug 21, 2021 16:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
